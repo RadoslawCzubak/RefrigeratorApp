768f3dbcafd0123ef878a9eae211ee5ed70a6c82
Radosław Czubak <radoslaw.marek.czubak@gmail.com>
2019-11-22 19:36:56 +0100

    on registration failed toast added

--- app/src/main/java/com/rczubak/refrigerator/RegistrationFragment.kt
+++ app/src/main/java/com/rczubak/refrigerator/RegistrationFragment.kt
@@ -1,26 +1,20 @@
 package com.rczubak.refrigerator
 
 
-import android.content.ContentValues.TAG
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.fragment_registration.*
-import kotlinx.android.synthetic.main.fragment_registration.emailTxt
-import kotlinx.android.synthetic.main.fragment_registration.loginBtnL
-import kotlinx.android.synthetic.main.fragment_registration.passTxt
-import kotlinx.android.synthetic.main.fragment_registration.registerBtnL
 
 
 class RegistrationFragment : Fragment() {
-    
+
     private lateinit var email: String
     private lateinit var password: String
     private lateinit var password2: String
@@ -49,7 +43,7 @@ class RegistrationFragment : Fragment() {
 
             println(email + " " + password + " " + password2)
             if (password != password2) {
-                Toast.makeText(this.context,R.string.password_diff , Toast.LENGTH_SHORT).show()
+                Toast.makeText(this.context, R.string.password_diff, Toast.LENGTH_SHORT).show()
 
             } else if (password.length < 8 || password.toLowerCase() == password) {
                 Toast.makeText(
@@ -59,7 +53,7 @@ class RegistrationFragment : Fragment() {
                 ).show()
             } else {
 
-                if (password2 != null && password != null && email != null) {
+                if (password2.isNotEmpty() && password.isNotEmpty() && email.isNotEmpty()) {
 
 
                     val view = it
@@ -76,7 +70,14 @@ class RegistrationFragment : Fragment() {
                                 .show()
                             Navigation.findNavController(view)
                                 .navigate(R.id.action_registrationFragment_to_loginFragment)
+                        }.addOnFailureListener {
+                            Toast.makeText(
+                                context,
+                                R.string.register_network_fail,
+                                Toast.LENGTH_SHORT
+                            ).show()
                         }
+
 
                 }
 
